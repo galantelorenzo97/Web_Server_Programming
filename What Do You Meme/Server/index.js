fd38ea4a890314b40a15ff459c32607adb79de9d
Lorenzo <galantelorenzo97@gmail.com>
2020-02-24 18:16:29 -0500

    Created navbar component; Added bulma via NPM; Created quotecard backend
    Updated Frontend Components; Updated backend

--- What Do You Meme/Server/index.js
+++ What Do You Meme/Server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+
+const quoteCards = require('./Game/quoteCards');
+
 const app = express();
 const port = 3000;
 
 //Dictate template to render or response to send
-app.get('/', (req,res) => res.send('Hello World'));
+app
+    .get('/', (req,res) => res.send('Hello World') )
+    .get('/game/quoteCards', (req,res) => res.send(quoteCards) );
 
 //Determine for which to listen to the machine.
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
--- What Do You Meme/client/src/views/Home.vue
+++ What Do You Meme/client/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="This class is Awesome"/>
-  </div>
+  <section class="container">
+    <div class="hero">
+        <h1 class="title">
+          Home Page
+        </h1>
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
