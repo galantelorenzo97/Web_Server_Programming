88defc5e3fb3283337bc1e9f0cadc8a654408794
Lorenzo <galantelorenzo97@gmail.com>
2020-02-29 12:36:31 -0500

    No display bug fix.
    Mistake in the code caused information not to route to the client. Turns out data was being relayed incorrectly.

--- What Do You Meme/client/src/models/Game.js
+++ What Do You Meme/client/src/models/Game.js
@@ -1,21 +1,23 @@
 export const Players = [
-    { name: 'Bernie', Score: 0, isDealer: false }
+    { Name: 'Bernie', Score: 0, isDealer: false }
 ];
 
-export const MyCards = []
+export const MyCards = [];
 
 export const PictureDeck = [
-    'http://www.dumpaday.com/wp-content/uploads/2020/02/00-131-750x280.jpg',
-    'http://www.dumpaday.com/wp-content/uploads/2020/02/00-123-750x280.jpg',
-    'http://www.dumpaday.com/wp-content/uploads/2019/02/00-50-620x280.jpg'
+    'http://www.dumpaday.com/wp-content/uploads/2020/02/00-147-750x280.jpg',
+    'http://www.dumpaday.com/wp-content/uploads/2020/02/00-146-750x280.jpg',
+    'http://www.dumpaday.com/wp-content/uploads/2020/02/00-131-750x280.jpg'
 ];
 
-export const CaptionDeck = [
-    "I have no captions so I typed this",
-    "I only export pain and pizza, and I'm all out of pizza",
-    "The face when there's a bug in the program",
-    "When you casually forget to patch a security hole in your program",
-    "Th- th- th- th- th- th- that's all folks!"
+export const CaptionsDeck = [
+    'For me, math class is like watching a foreign movie without subtitles.',
+    'Maybe if we tell people the brain is an app, they will start using it.',
+    'When nothing goes right, go left.',
+    'A cop pulled me over and told me “Papers”, so I said “Scissors, I win!” and drove off.',
+    'Mom: Why is everything on the floor? Me: Gravity!',
+    'Sure, I do marathons. On Netflix.',
+    'Did it for the memories – totally worth it!',
 ];
 
 export let CurrentPicture = "";
@@ -23,8 +25,8 @@ export let CurrentPicture = "";
 export const CardsInPlay = [];
 
 export function Init(){
-    MyCards.push(CaptionDeck[0]);
-    MyCards.push(CaptionDeck[1]);
+    MyCards.push(CaptionsDeck[0])
+    MyCards.push(CaptionsDeck[1]);
 
     CurrentPicture = PictureDeck[0];
-}
+} 
--- What Do You Meme/client/src/views/Home.vue
+++ What Do You Meme/client/src/views/Home.vue
@@ -1,32 +1,37 @@
 <template>
   <section class="container">
     <div class="hero">
-        <h1 class="title">
-          Home Page
-        </h1>
+      <h1 class="title">Home Page</h1>
     </div>
     <div class="card">
       <div class="card-content">
         <ul>
-          <li v-for="player in Players" :key="player.Name">
-            {{ player.Name }} {{ player.Score }}
-          </li>
+          <li v-for="player in Players" :key="player.Name">{{ player.Name }} {{ player.Score }}</li>
         </ul>
       </div>
+
+      <img :src="CurrentPicture" class="card-image" />
     </div>
   </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import {Players, PictureDeck, CurrentPicture, Init } from "../models/Game";
+import { Players, PictureDeck, CurrentPicture, Init } from "../models/Game";
 Init();
 export default {
-  name: 'Home',
-  components: {
+  name: "Home",
+  data: () => ({
     Players,
     PictureDeck,
     CurrentPicture
-  }
-}
+  }),
+  components: {}
+};
 </script>
+
+<style>
+img.card-image {
+  width: 100%;
+}
+</style> 
