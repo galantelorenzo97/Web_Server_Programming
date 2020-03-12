acf91af3f4e3b4e38c25c7e2e41496b067c4fe00
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2020-03-12 15:19:21 -0400

    Worked some more on backend

--- Server/controllers/game.js
+++ Server/controllers/game.js
@@ -5,6 +5,10 @@ const quoteCards = require('../models/quoteCards');
 const router = express.Router();
 
 router
+    .use('/quoteCards', (req,res, next) => {
+        console.log({method: req.method, query: req.query});
+        next();
+    })
     .get('/quoteCards', (req, res) => res.send(quoteCards) )
     .post('/quoteCards', (req, res) => {
         quoteCards.add(req.query.text);
--- Server/index.js
+++ Server/index.js
@@ -4,10 +4,19 @@ const gameController = require('./controllers/game');
 const app = express();
 const port = 3000;
 
+
 //Dictate template to render or response to send
 app
+    .use(express.json())
+    .use(express.urlencoded({ extended : true }))
+    .use(express.static(__dirname + '/../client/dist'))
     .get('/', (req,res) => res.send('Hello World') )
-    .use('/game', gameController);
+    .use('/game', gameController)
 
+    /* FIX CATCH ALL
+    .use((req,res) => {
+        const homePath = joi 
+        res.sendFile(__dirname + '/../client/dist/index.html')});
+        */
 //Determine for which to listen to the machine.
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
